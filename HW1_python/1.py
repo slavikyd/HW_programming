f1e4dfee1527098e582ba430282ba359a8f7f83f
slavikyd <47947352+slavikyd@users.noreply.github.com>
2023-02-21 14:56:25 +0300

    Update 1.py

--- HW1_python/1.py
+++ HW1_python/1.py
@@ -1,10 +1,10 @@
 k = int(input())
-m = int(input())
+m = int(input()) #ввод
 n = int(input())
-if n > k:
+if n > k: #если n < k то нам будет нужно больше одной итерации жарки
     iter = n // k
     out = m * 2 * n * iter
     print(out)
 else:
-    out = n * m * 2
+    out = n * m * 2 #иначе нам хватит одной итерации 
     print(out)
